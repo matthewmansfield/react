dd40dece5b0f5e46a1fc52cc753969947eac19e2
Matthew Mansfield <matthew.mansfield@masterelectronics.com>
2023-08-18 14:14:30 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+constructor() {
+super();
 
+
+this.state = {
+  name: 'Beavis'
+}
+}
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi Matthew new
+            Hi {this.state.name}
           </p>
         <button>Change Name</button>
         </header>
