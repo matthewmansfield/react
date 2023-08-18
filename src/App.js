b9a36ec66b7b93396faed634e556160211ba69f1
Matthew Mansfield <matthew.mansfield@masterelectronics.com>
2023-08-18 15:15:32 -0700

    updates before API

--- src/App.js
+++ src/App.js
@@ -7,41 +7,38 @@ class App extends Component {
 constructor() {
 super();
 
-this.state = {
-  name: {firstName: 'Jack', lastName: 'Hugh'},
-  company: 'ZTM'
-}
+  this.state = {
+    monsters: [
+      {
+        name: 'Linda',
+        id: 'eke345323'
+      },
+      {
+        name: 'Frank',
+        id: 'emmnde22918323'
+      },
+      {
+        name: 'Jacky',
+        id: 'euewgbde312567623'
+      },
+      {
+        name: 'Beavis',
+        id: 'dgyeb345323'
+      }
+    ]
+   
+  };
 }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
-          </p>
-          <button
-          onClick={() =>{
-            this.setState(() => {
-              return {
-              name: {firstName: 'Beavis', lastName: 'Smith'},
-              } 
-            }, () => {
-              console.log(this.state);
-            });
-          }}>
-          Change Name
-          </button>
-
-
-        {/* <button onClick={() => {
-          this.setState({ 
-            name: {firstName: 'Beavis', lastName: 'Smith'} });
-          console.log(this.state);
-        }}>Change Name</button> */}
-        </header>
-      </div>
-    );
+    return <div className="App">
+      {
+        this.state.monsters.map((monster) => {
+          return <div key={monster.id}>
+            <h1>{monster.name}</h1>
+            </div>;
+        })
+      }
+    </div>;
   }
 }
 
