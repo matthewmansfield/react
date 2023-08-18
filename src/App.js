c9642d588ca41dc873ee3bed37e4af48c44599c1
Matthew Mansfield <matthew.mansfield@masterelectronics.com>
2023-08-18 09:28:40 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi Matthew
-        </p>
-      <button>Change Name</button>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi Matthew
+          </p>
+        <button>Change Name</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
