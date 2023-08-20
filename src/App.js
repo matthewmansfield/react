70084033e0b1a6212f53b879f2e0a3a21fdab0e2
Matthew Mansfield <matthew.mansfield@masterelectronics.com>
2023-08-20 09:05:34 -0700

    with search strings

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ super();
   this.state = {
     monsters: [],
   };
-  console.log('1')
+  console.log('constructor')
 }
 
 componentDidMount(){
-  console.log('3')
+  console.log('componentDidMount')
   fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
     .then((users) => 
@@ -30,16 +30,34 @@ componentDidMount(){
 }
 
   render() {
-    console.log('2')
-    return <div className="App">
-      {
-        this.state.monsters.map((monster) => {
-          return <div key={monster.id}>
-            <h1>{monster.name}</h1>
-            </div>;
-        })
-      }
-    </div>;
+    console.log('render');
+    return (
+      <div className="App">
+        <input 
+        className='search-box' 
+        type='search' 
+        placeholder='search monsters' 
+        onChange={(event) => {
+          console.log(event.target.value);
+          const searchString = event.target.value.toLocaleLowerCase();
+          const filteredMonsters = this.state.monsters.filter((monster) => {
+          return monster.name.includes(event.target.value);
+        });
+
+          this.setState(() => {
+            return { monsters: filteredMonsters };
+          })
+        }}
+        />
+        {this.state.monsters.map((monster) => {
+            return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+              </div>
+            );
+          })}
+      </div>
+    );
   }
 }
 
