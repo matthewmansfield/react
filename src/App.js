06f59452ac88d55f29ff0a015d54ce6fe5766917
Matthew Mansfield <matthew.mansfield@masterelectronics.com>
2023-08-20 14:16:51 -0700

    organizing code

--- src/App.js
+++ src/App.js
@@ -30,11 +30,22 @@ componentDidMount(){
     );
 }
 
+onSearchChange = (event) => {
+  console.log(event.target.value);
+  const searchField = event.target.value.toLocaleLowerCase();
+  this.setState(
+    () => {
+    return { searchField };
+    });
+}
+
   render() {
     console.log('render');
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
@@ -43,14 +54,7 @@ componentDidMount(){
         className='search-box' 
         type='search' 
         placeholder='search monsters' 
-        onChange={(event) => {
-          console.log(event.target.value);
-          const searchField = event.target.value.toLocaleLowerCase();
-          this.setState(
-            () => {
-            return { searchField };
-            });
-        }}
+        onChange={onSearchChange}
         />
         {filteredMonsters.map((monster) => {
             return (
